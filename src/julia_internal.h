aa7a3ea7a4d98488316472d90a03667d1b7e4aa3
Nicholas Bauer <nicholasbauer@outlook.com>
2021-06-04 09:11:35 -0400

    works when it doesn't segfault

--- src/julia_internal.h
+++ src/julia_internal.h
@@ -455,6 +455,8 @@ JL_DLLEXPORT jl_code_info_t *jl_new_code_info_uninit(void);
 void jl_resolve_globals_in_ir(jl_array_t *stmts, jl_module_t *m, jl_svec_t *sparam_vals,
                               int binding_effects);
 
+jl_code_info_t *jl_get_code_info(jl_method_instance_t *methodinst);
+
 int jl_valid_type_param(jl_value_t *v);
 
 JL_DLLEXPORT jl_value_t *jl_apply_2va(jl_value_t *f, jl_value_t **args, uint32_t nargs);
